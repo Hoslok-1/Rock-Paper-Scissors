83dc1e85622f4d05c08bef5edf733967e99f7873
Anuj Hoslok <anujhoslokdev@gmail.com>
2022-07-17 17:44:45 +0530

    added very basic UI

--- test1.js
+++ test1.js
@@ -2,29 +2,46 @@ function getComputerChoice()
 {
     let array = ['rock','paper','scissors'];
     let guess = Math.floor(Math.random() * 3);
-        
-    console.log(array[guess]);
+    
     return array[guess];
     
 }
 
+// function game()
+// {
+//      for(i = 0;i<5;i++)
+//      {
+//           playerSelection = 'rock';
+//           getComputerChoice();
+//           computerSelection = getComputerChoice();
+//           console.log(playRound(playerSelection,computerSelection));
+//      }
+// }
+let count = 0;
 
 
-function game()
-{
-     for(i = 0;i<5;i++)
-     {
-          playerSelection = prompt("enter your move");
-          getComputerChoice();
-          computerSelection = getComputerChoice();
-          console.log(playRound(playerSelection,computerSelection));
-     }
-}
+let selectRock = document.getElementById('selectRock');
+selectRock.addEventListener('click',function(){
+     count++;
+     console.log(playRound('rock',getComputerChoice()));
+});
 
+let selectPaper = document.getElementById('selectPaper');
+selectPaper.addEventListener('click',function(){
+     count++;
+     console.log(playRound('paper',getComputerChoice()));
+});
+
+let selectScissors = document.getElementById('selectScissors');
+selectScissors.addEventListener('click',function(){
+     count++;
+     console.log(playRound('scissors',getComputerChoice()));
+});
 
 
 function playRound(playerSelection,computerSelection)
 {
+     
      if(playerSelection == 'rock' && computerSelection == 'rock')
      {
           decision = 'Its a tie! Go again.';
@@ -66,9 +83,5 @@ function playRound(playerSelection,computerSelection)
           decision = 'something went wrong';
      }
 
-     return decision
+     return decision;
 }
-
-game();
-
-
